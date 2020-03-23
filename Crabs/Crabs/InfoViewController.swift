4ed7a4f82cac2c8f3cfdf6a8955c0f791c66924b
Cody Morley <codymorleyinc@gmail.com>
2020-03-23 14:27:04 -0700

    added info view layout and fixed auto-layout issues in main.storyboard

--- Crabs/Crabs/InfoViewController.swift
+++ Crabs/Crabs/InfoViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
